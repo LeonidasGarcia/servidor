add34040b7b7e4a372616d332348161797c517ae
Leo <leonidasgarcialescano@gmail.com>
2024-09-17 03:14:29 -0500

    owo2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.get("/api/miwebo", (req, res)=>{
 
     console.log("he recibido una solicitud get desde react");
-    res.text().send("Alberto traga pene");
+    res.send("Alberto traga pene");
 })
 
 app.listen(port, ()=>{
