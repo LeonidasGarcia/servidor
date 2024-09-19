6491820a5b61cfd11562f0915fd98871a0815829
Leo <leonidasgarcialescano@gmail.com>
2024-09-19 03:07:04 -0500

    ola

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors"
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: "http://localhost:5173/"
+}))
 
 app.get("/api/", (req, res)=>{
 
