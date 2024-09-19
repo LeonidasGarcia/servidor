5f31bc8f7389accb5324c05b030577f87f0b721e
Leo <leonidasgarcialescano@gmail.com>
2024-09-19 02:04:09 -0500

    añadiendo cors

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import cors from "cors"
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors(
+    {
+        origin: "https://leonidasgarcia.github.io"
+    }
+))
 
 app.get("/api/", (req, res)=>{
 
